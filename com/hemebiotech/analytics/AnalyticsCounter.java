7f66026a7bea685c942c78b42a58c86b4c6b33f4
Sumeyra Dosemetas <991c8rczfsk7-student@anonymous-codio.com>
2023-03-01 23:09:19 +0000

    refactor analytics counter

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,13 +3,24 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.*;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	
 	private static int rashCount = 0;		
 	private static int pupilCount = 0;		
+
+  private static ISymptomWriter writer;
+  private static ISymptomReader reader;
+
+  public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer){
+    this.reader = reader;
+    this.writer = writer;
+  }
 	
 	public static void main(String args[]) throws Exception {
+
+   
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
@@ -40,4 +51,53 @@ public class AnalyticsCounter {
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
 	}
+
+  public List<String> getSymptoms(){
+    return reader.getSymptoms();
+  }
+
+  public Map<String,Integer> countSymptoms(List<String> symptoms){
+    Map<String,Integer> mapList =new HashMap<>();
+		int quantity=1;
+		for ( int i = 0; i < symptoms.size(); i++) {
+			String field = symptoms.get(i);
+			for(int j = i; j < symptoms.size(); j++) {
+				if(field.equals(symptoms.get(j))) {
+					quantity  +=1;
+				}
+				else
+					continue;
+			}
+			if(!mapList.containsKey(field))
+			{
+				mapList.put(field, quantity);
+			}
+			quantity = 1;
+		}
+		return mapList;
+	}
+
+ 
+  public Map<String,Integer> sortSymptoms(Map<String,Integer> symptoms){
+    Map<String,Integer> mapSorted = new LinkedHashMap<String,Integer>();
+		List<String> symptomsOnly = new ArrayList<String>();
+		for(Map.Entry<String, Integer> c : symptoms.entrySet()) {
+			symptomsOnly.add(c.getKey());
+		}
+		Collections.sort(symptomsOnly);
+		int i = 0;
+		while( i<symptomsOnly.size()) {
+			mapSorted.put(symptomsOnly.get(i), symptoms.get(symptomsOnly.get(i)));
+			i++;
+		}
+		return mapSorted;
+	}
+  
+
+  public void writeSymptoms(Map<String,Integer> symptoms){
+    writer.writeSymptoms(symptoms);
+  }
+
+
+
 }
